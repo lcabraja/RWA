820173ea92ae6b43c6af988033d0ed3809ca113a
Luka Cabraja <luka@cabraja.eu>
2021-06-04 17:07:02 +0200

    vjezbe04 zadatak04 complete vjezbe04 complete

--- Vjezbe03/Zadatak04/Default.aspx
+++ Vjezbe03/Zadatak04/Default.aspx
@@ -5,14 +5,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Zadatak04</title>
-        <style>
+    <style>
         .container {
             width: 400px;
             margin: auto;
             margin-top: 10rem;
         }
+
         .wide {
             width: 150px;
+            height: 250px;
         }
     </style>
 </head>
@@ -31,16 +33,15 @@
                 </tr>
                 <tr>
                     <td>
-                        <asp:ListBox CssClass="wide" ID="onOffer" runat="server" SelectionMode="Single">
-                        </asp:ListBox>
+                        <asp:ListBox CssClass="wide" ID="onOffer" runat="server" SelectionMode="Single" OnSelectedIndexChanged="onOffer_SelectedIndexChanged" />
                     </td>
                     <td>
-                        <asp:Button ID="btLeft" Text="<" runat="server" OnClick="btLeft_Click"/>
+                        <asp:Button ID="btLeft" Text="<" runat="server" OnClick="btLeft_Click" />
                         <br />
                         <asp:Button ID="btRight" Text=">" runat="server" OnClick="btRight_Click" />
                     </td>
                     <td>
-                        <asp:ListBox CssClass="wide" ID="selected" runat="server" SelectionMode="Single"/>
+                        <asp:ListBox CssClass="wide" ID="selected" runat="server" SelectionMode="Single" OnSelectedIndexChanged="selected_SelectedIndexChanged"/>
                     </td>
                 </tr>
             </table>
--- Vjezbe03/Zadatak04/Default.aspx.cs
+++ Vjezbe03/Zadatak04/Default.aspx.cs
@@ -11,38 +11,83 @@ namespace Zadatak04
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.Cookies["pero"] == null) {
+            if (Request.Cookies["pero"] == null)
+            {
                 Response.Redirect("Login.aspx");
             }
 
-            var onoffer = Request.Cookies["onoffer"];
-            var selected = Request.Cookies["selected"];
-
-            var offc = onoffer.Values.Count;
-            var selc = selected.Values.Count;
-
-            for (int i = 0; i < offc; i++)
+            if (Session["onOffer"] == null && Session["selected"] == null)
             {
-                onOffer.Items.Add(onoffer[$"item{i}"]);
+                CreateItems();
             }
-            for (int i = 0; i < selc; i++)
+            if (!IsPostBack)
             {
-                onOffer.Items.Add(selected[$"item{i}"]);
+                CreateListItems();
             }
+        }
+
+        private void CreateListItems()
+        {
+            var sessionOffer = Session["onOffer"] as List<string>;
+            var sessionSelected = Session["selected"] as List<string>;
+
+            //onOffer.Items.Clear();
+            //selected.Items.Clear();
+
+            sessionOffer.ForEach(x => onOffer.Items.Add(new ListItem(x, x)));
+            sessionSelected.ForEach(x => selected.Items.Add(new ListItem(x, x)));
+            Response.Write("1st");
+        }
+
+        private void CreateItems()
+        {
+            Session["onOffer"] = new List<string>
+            {
+                "Car",
+                "Bike",
+                "Skiis",
+                "Rollerblades",
+                "Motorcycle",
+                "Segway",
+                "Hoverboard",
+                "Running Shoes"
+            };
+            Session["selected"] = new List<string>();
         }
 
         protected void btLeft_Click(object sender, EventArgs e)
         {
-            var transfer = selected.SelectedItem;
+            var sessionOffer = Session["onOffer"] as List<string>;
+            var sessionSelected = Session["selected"] as List<string>;
+
+            var transfer = selected.SelectedValue;
+            sessionSelected.Remove(transfer);
+            sessionOffer.Add(transfer);
+
             selected.Items.Remove(transfer);
-            onOffer.Items.Add(transfer);
+            onOffer.Items.Add(new ListItem(transfer, transfer));
         }
 
         protected void btRight_Click(object sender, EventArgs e)
         {
-            var transfer = onOffer.SelectedItem;
+            var sessionOffer = Session["onOffer"] as List<string>;
+            var sessionSelected = Session["selected"] as List<string>;
+
+            var transfer = onOffer.SelectedValue;
+            sessionOffer.Remove(transfer);
+            sessionSelected.Add(transfer);
+
             onOffer.Items.Remove(transfer);
-            selected.Items.Add(transfer);
+            selected.Items.Add(new ListItem(transfer, transfer));
+        }
+        protected void onOffer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected.ClearSelection();
+        }
+
+        protected void selected_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            onOffer.ClearSelection();
         }
     }
 }
